737657532ef991f741dcbed5f088806053a3ae8a
TerryRoberts1995 <terryjbroberts@yahoo.com>
2020-08-19 01:41:33 -0500

    added times and content to modal

--- src/components/pages/Schedule.js
+++ src/components/pages/Schedule.js
@@ -3,18 +3,22 @@ import axios from 'axios';
 import Calendar from 'react-calendar';
 import Modal from 'react-modal';
 import 'react-calendar/dist/Calendar.css';
+import * as Cookies from 'js-cookie';
 
 export default function Schedule() {
 
     const [apiEvent, setApiEventData] = useState([]);
-    // const [events, setEvents] = useState([]);
     const [isOpen, setIsOpen] = useState(false);
     const [choice, setChoice] = useState("");
-    const [title, setTitle] = useState("");
     const [trueChoice, setTrueChoice] = useState("");
+    const [title, setTitle] = useState("");
     const [month, setMonth] = useState(0);
     const [year, setYear] = useState(0);
     const [day, setDay] = useState(0);
+    const [newTime, setNewTime] = useState("");
+    const [newTitle, setNewTitle] = useState("");
+    const [newDate, setNewDate] = useState("");
+    const [time, setTime] = useState("");
 
     const customStyles = {
         content: {
@@ -41,17 +45,45 @@ export default function Schedule() {
 
     const handleEventCheck = () => {
         let counter = 1;
+
         const allTitles = apiEvent.map(e => {
 
-            console.log(new Date(e.date).toDateString(), 'event') // Test
-            console.log(choice, 'choice') // Test
-
             if (new Date(e.date).toDateString() === choice) {
-                return <div key={counter++}>{e.title}</div>
+                console.log('date set');
+
+                return <div className="title-container" key={counter++}> Event: {e.title}</div>
             }
         })
 
+        const allTimes = apiEvent.map(e => {
+            if (new Date(e.date).toDateString() === choice) {
+                console.log('time set');
+
+                return <div className="time-container" key={counter++}> Starts: {e.time}</div>
+            }
+        })
+
+        setTime(allTimes);
         setTitle(allTitles);
+    }
+
+    const sendEvent = (event) => {
+        event.preventDefault();
+
+        console.log(event)
+        axios.post("https://sleepless-api.herokuapp.com/events",
+            buildForm(),
+            {
+                headers: {
+                    Authorization: `Bearer ${Cookies.get("access_token_cookie")}`,
+                    'X-CSRF-TOKEN': `${Cookies.get("csrf_access_token")}`
+                }, withCredentials: true
+            })
+
+            .then(res => {
+                setIsOpen(false);
+            })
+            .catch(error => console.log('You api request has failed', error));
     }
 
     const getEventData = () => {
@@ -62,10 +94,6 @@ export default function Schedule() {
             })
             .catch(error => console.log("A request to your API has resulted in a failure. ", error));
     }
-
-    // const destructorEvent = () => {
-    //      setEvents(apiEvents);
-    // }
 
     useEffect(() => {
         getEventData()
@@ -78,7 +106,13 @@ export default function Schedule() {
     }, [isOpen])
 
     const handleModalChange = (event) => {
-        console.log(event.target.value)
+
+        const trueDate = new Date(choice).toString();
+
+        if (event.target.name === "title") {
+            setNewTitle(event.target.value);
+            setNewDate(trueDate);
+        }
     }
 
     const handleSelect = (event) => {
@@ -96,15 +130,27 @@ export default function Schedule() {
         setYear(0);
     }
 
+    const buildForm = () => {
+        var formData = new FormData();
 
+        formData.append("title", newTitle);
+        formData.append("date", new Date(choice));
+        formData.append("time", newTime);
 
-    // console.log(choice)
+        return formData;
+
+    }
+
+    const handleTime = (event) => {
+        setNewTime(event.target.value);
+        setTime(event.target.value);
+    }
+
     return (
         <div className="calendar-container">
             <div className="calender">
                 <Calendar
                     onChange={handleModalClose}
-                    // onClickDay={handleEventCheck}
                     value={choice ? new Date(choice) : new Date()}
                     defaultView={'month'}
                     defaultActiveStartDate={new Date()}
@@ -125,20 +171,27 @@ export default function Schedule() {
                         <div>{`${month + 1}/${day}/${year}`}</div>
 
 
-                        <form className="add-delete-image-form" action="/images" type="submit">
+                        <form className="add-delete-image-form" action="/events" type="submit" onSubmit={sendEvent}>
 
                             <div className="form-input-wrapper">
                                 <div className="add-input">
-                                    <input className="modal-inputs" type="text" placeholder="Event Title" name="title" onChange={event => handleModalChange(event)} />
+                                    <input className="modal-inputs" type="text" placeholder="Event Title" name="title" onChange={(event) => handleModalChange(event)} />
                                 </div>
 
                                 <div className="add-input">
-                                    <input className="modal-inputs" type="time" placeholder="Event Time" name="time" onClick={(event) => console.log(event.target.value)} />
+                                    <input className="modal-inputs" type="time" placeholder="Event Time" name="time" onChange={(event) => { handleTime(event) }} />
                                 </div>
                             </div>
                             <div className="events-container">
-                                {title}
+                                <div className="times">
+                                    {time}
+                                </div>
+
+                                <div className="titles">
+                                    {title}
+                                </div>
                             </div>
+                            <button>Add Event</button>
                         </form>
                     </div>
 
