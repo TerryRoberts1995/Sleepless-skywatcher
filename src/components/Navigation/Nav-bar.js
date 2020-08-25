76d9322ed849a79c3b7af424d0aeb5344eb05b7c
TerryRoberts1995 <terryjbroberts@yahoo.com>
2020-08-25 17:44:23 -0500

    added screen sizer, started building mobile burger menu

--- src/components/Navigation/Nav-bar.js
+++ src/components/Navigation/Nav-bar.js
@@ -1,8 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { A } from 'hookrouter';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 
 export default function Navbar() {
+
+    const useWindowSize = () => {
+
+        const [windowSize, setWindowSize] = useState({
+            width: undefined,
+            height: undefined,
+        });
+
+        useEffect(() => {
+
+            function handleResize() {
+
+                setWindowSize({
+                    width: window.innerWidth,
+                    height: window.innerHeight,
+                });
+            }
+
+            window.addEventListener("resize", handleResize);
+
+            handleResize();
+
+            return () => window.removeEventListener("resize", handleResize);
+        }, []);
+
+        return windowSize;
+    }
+
+
+    const size = useWindowSize();
+
     return (
         <div className="navbar-wrapper">
             <div className="logo-container">
@@ -10,6 +42,11 @@ export default function Navbar() {
             </div>
 
             <div className="nav-link-wrapper">
+
+                <div className="mobile-nav-button">
+                    <FontAwesomeIcon onClick={} icon="bars" />
+                </div>
+
                 <div className="nav-link">
                     <A className="link" href="/">Home</A>
                 </div>
--- src/icons.js
+++ src/icons.js
@@ -1,12 +1,13 @@
 // import the library
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faPlusSquare, faMinusSquare } from '@fortawesome/free-solid-svg-icons';
+import { faPlusSquare, faMinusSquare, faBars } from '@fortawesome/free-solid-svg-icons';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 
 const Icons = () => {
     return library.add(
         faPlusSquare,
         faMinusSquare,
+        faBars,
         fab
 
     );
